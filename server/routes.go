6b998c18a40974dd788c006a56ee765d90e1ef09
Sebastian Döll <sebastian@katallaxie.dev>
2025-03-06 14:35:41 +0100

    chore: export RegistryOptions

--- server/routes.go
+++ server/routes.go
@@ -592,7 +592,7 @@ func (s *Server) PullHandler(c *gin.Context) {
 			ch <- r
 		}
 
-		regOpts := &registryOptions{
+		regOpts := &RegistryOptions{
 			Insecure: req.Insecure,
 		}
 
@@ -641,7 +641,7 @@ func (s *Server) PushHandler(c *gin.Context) {
 			ch <- r
 		}
 
-		regOpts := &registryOptions{
+		regOpts := &RegistryOptions{
 			Insecure: req.Insecure,
 		}
 
